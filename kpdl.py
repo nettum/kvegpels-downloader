db0a3acc0535ba1e1b966f09bc880dde1cc9c03f
Marius Nettum <nettum@gmail.com>
2020-03-11 23:14:30 +0100

    Fix typos and text

--- kpdl.py
+++ kpdl.py
@@ -8,22 +8,22 @@ from time import sleep
 # But no one has seen or heard from him since last Wednesday
 today = date.today()
 delta = offset = (today.weekday() + 4) % 7 + 1
-lastWedensday = today - timedelta(days=delta)
+lastWednesday = today - timedelta(days=delta)
 
 downloadPath = path.dirname(path.realpath(__file__)) + '/downloads/'
 numFailedRequests = 0
 weekDiffs = 0
 
 while numFailedRequests < 10:
-    dlDate = lastWedensday - timedelta(weeks=weekDiffs)
+    dlDate = lastWednesday - timedelta(weeks=weekDiffs)
     filename = 'kvegpels_' + dlDate.strftime('%Y%m%d') + '.mp3'
     weekNumber = dlDate.strftime('%V')
     year = dlDate.strftime('%Y')
     dlUrl = f"http://dread.radionova.no/kringlast/kvegpels/{filename}"
     if path.exists(downloadPath + filename):
-        print(f"Skip download of kvegpels week {weekNumber}, {year}: {dlUrl}, file already exists")
+        print(f"Skip download of kvegpels week {weekNumber} {year}, {filename} already exists")
     else:
-        print(f"Downloading kvegpels week {weekNumber}, {year}: {dlUrl}")
+        print(f"Downloading kvegpels week {weekNumber} {year}: {dlUrl}")
         response = requests.get(dlUrl, stream=True)
         if response.status_code == 200:
             with open(downloadPath + filename, 'wb') as file:
@@ -33,7 +33,7 @@ while numFailedRequests < 10:
                         file.write(chunk)
                         file.flush()               
         else:
-            print(f"Could not download kvegpels week {weekNumber}, {year}: {dlUrl}, server returned status {response.status_code}")
+            print(f"Could not download kvegpels week {weekNumber} {year}: {dlUrl}, server returned status {response.status_code}")
             numFailedRequests += 1
     
     weekDiffs += 1
